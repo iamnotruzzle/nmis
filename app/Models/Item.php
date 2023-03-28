942f29eb5d44325bbc0d185d059af918c016c83f
iamnotruzzle <iamnotruzzle@gmail.com>
2023-03-28 13:25:01 +0800

    created item model

---
+++ app/Models/Item.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hclass2';
+    protected $primaryKey = 'cl2comb';
+    public $incrementing = false;
+    // declare primary as string
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'cl2comb',
+        'cl1comb',
+        'cl2code',
+        'stkno',
+        'cl2desc',
+        'cl2retprc',
+        'uomcode',
+        'cl2dteas',
+        'cl2stat',
+        'cl2lock',
+        'cl2upsw',
+        'cl2dtmd',
+        'curcode',
+        'cl2purp',
+        'curcode1',
+        'uomcode1',
+        'cl2ctr',
+        'brandname',
+        'stockbal',
+        'pharmaceutical',
+        'baldteasof',
+        'begbal',
+        'lot_no',
+        'barcode',
+        'rpoint',
+    ];
+
+    public function category()
+    {
+        return $this->hasOne(Category::class, 'cl1comb', 'cl1comb');
+    }
+}
