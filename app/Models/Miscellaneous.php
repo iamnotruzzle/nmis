7281bab5bef42b9037b75e66c0ca23cf0de8d216
iamnotruzzle <iamnotruzzle@gmail.com>
2023-04-25 13:45:17 +0800

    created miscellaneous model

---
+++ app/Models/Miscellaneous.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Miscellaneous extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hmisc';
+    protected $primaryKey = 'hmcode';
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'hmcode',
+        'hmdesc',
+        'hmdteas',
+        'hmamt',
+        'hmstat',
+        'hmlock',
+        'updsw',
+        'datemod',
+        'uomcode', // UNIT
+        'srcchrg',
+        'priority',
+        'discount'
+    ];
+}
