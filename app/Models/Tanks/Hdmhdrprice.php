ad09111c04e36c679f5657717feaa0eccd807414
iamnotruzzle <iamnotruzzle@gmail.com>
2023-05-19 10:44:03 +0800

    created hdmhdrprice price

---
+++ app/Models/Tanks/Hdmhdrprice.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Tanks;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hdmhdrprice extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hdmhdrprice';
+    public $incrementing = false;
+    // declare primary as string
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'dmdcomb',
+        'dmdctr',
+        'dmhdrsub',
+        'dmduprice',
+        'unitcode',
+        'dmdrem',
+        'dmdprdte',
+        'dmselprice',
+        'stockbal',
+        'expdate',
+        'brandname',
+        'stock_id',
+    ];
+}
