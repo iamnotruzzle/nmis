35f7cbe30d860904d3ab7bf94a2d6bc5354774f6
iamnotruzzle <iamnotruzzle@gmail.com>
2023-05-24 13:21:36 +0800

    created patient_bill_logs table

---
+++ database/migrations/2023_05_24_131109_create_csrw_patient_bill_logs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCsrwPatientBillLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csrw_patient_bill_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('enccode')->nullable();
+            $table->string('acctno')->nullable();
+            $table->bigInteger('ward_stocks_id')->nullable();
+            $table->string('itemcode')->nullable();
+            $table->bigInteger('batch_no')->nullable();
+            $table->dateTime('manufactured_date')->nullable();
+            $table->dateTime('delivery_date')->nullable();
+            $table->dateTime('expiration_date')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('price_per_piece')->nullable();
+            $table->integer('price_total')->nullable();
+            $table->dateTime('charge_date')->nullable();
+            $table->string('entry_at')->nullable(); // wardcode
+            $table->string('entry_by')->nullable(); // employeeid
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csrw_patient_bill_logs');
+    }
+}
