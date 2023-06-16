ef98c78b8f4e8bd7d2cd13cf3757cec9ac112678
iamnotruzzle <iamnotruzzle@gmail.com>
2023-06-16 09:14:15 +0800

    created ward transfer stock table

---
+++ database/migrations/2023_06_16_091045_create_csrw_ward_transfer_stock_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCsrwWardTransferStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csrw_ward_transfer_stock', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('ward_stock_id');
+            $table->string('from');
+            $table->string('to');
+            $table->string('requested_by');
+            $table->string('approved_by');
+            $table->string('remarks', 500);
+            $table->string('status'); // transferred or received
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csrw_ward_transfer_stock');
+    }
+}
