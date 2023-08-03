5a48d8f4bc0576244d7c54d063ce91f6690ff2a5
iamnotruzzle <iamnotruzzle@gmail.com>
2023-08-03 11:20:28 +0800

    breakpoint

--- app/Http/Controllers/Csr/Inventory/Stocks/CsrStocksController.php
+++ app/Http/Controllers/Csr/Inventory/Stocks/CsrStocksController.php
@@ -78,27 +78,6 @@ class CsrStocksController extends Controller
         // brands
         $brands = Brand::get();
 
-        //////////////////////////////////////////////////
-        $report = [];
-
-        $csr_report = DB::table('csrw_csr_stocks')
-            ->join('hclass2', 'csrw_csr_stocks.cl2comb', '=', 'hclass2.cl2comb')
-            ->leftJoin('huom', 'csrw_csr_stocks.uomcode', '=', 'huom.uomcode')
-            ->select('hclass2.cl2comb', 'hclass2.cl2desc', 'huom.uomdesc', DB::raw('SUM(csrw_csr_stocks.quantity) as quantity'))
-            // ->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])
-            ->groupBy('hclass2.cl2comb', 'hclass2.cl2desc', 'huom.uomdesc')
-            ->get();
-
-        $ward_report_from_csr =
-            DB::table('csrw_wards_stocks')
-            ->select('cl2comb', DB::raw('SUM(quantity) as quantity'))
-            ->where('from', 'CSR')
-            ->groupBy('cl2comb')
-            ->get();
-
-        // dd($ward_report_from_csr);
-        //////////////////////////////////////////////////
-
         return Inertia::render('Csr/Inventory/Stocks/Index', [
             'items' => $items,
             'stocks' => $stocks,
