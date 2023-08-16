818c239594920f15b914615e830c92e3696ad088
iamnotruzzle <iamnotruzzle@gmail.com>
2023-08-16 09:31:36 +0800

    added header colors for csr reports

--- resources/js/Pages/Csr/Reports/Index.vue
+++ resources/js/Pages/Csr/Reports/Index.vue
@@ -22,71 +22,75 @@
         <tr>
           <td
             rowspan="2"
-            class="group-header"
+            class="group-header bg-white colored-header"
           >
             ITEM DESCRIPTION
           </td>
           <td
             rowspan="2"
-            class="group-header"
+            class="group-header bg-white colored-header"
           >
             UNIT
           </td>
           <td
             rowspan="2"
-            class="group-header"
+            class="group-header bg-white colored-header"
           >
             UNIT COST
           </td>
           <th
+            colspan="2"
             scope="colgroup"
-            class="group-header"
+            class="group-header bg-yellow-400 colored-header"
           >
             CSR
           </th>
           <th
+            colspan="2"
             scope="colgroup"
-            class="group-header"
+            class="group-header bg-yellow-400 colored-header"
           >
             WARDS
           </th>
           <th
             colspan="2"
             scope="colgroup"
-            class="group-header"
+            class="group-header bg-yellow-400 colored-header"
           >
             TOTAL BEGINNING BALANCE
           </th>
           <th
             colspan="2"
             scope="colgroup"
-            class="group-header"
+            class="group-header bg-green-400 colored-header"
           >
             SUPPLIES ISSUED TO WARDS
           </th>
           <th
             colspan="2"
             scope="colgroup"
-            class="group-header"
+            class="group-header bg-yellow-600 colored-header"
           >
             CONSUMPTION
           </th>
           <th
+            colspan="2"
             scope="colgroup"
-            class="group-header"
+            class="group-header bg-cyan-400 colored-header"
           >
             CSR
           </th>
           <th
+            colspan="2"
             scope="colgroup"
-            class="group-header"
+            class="group-header bg-cyan-400 colored-header"
           >
             WARDS
           </th>
           <th
             colspan="2"
             scope="colgroup"
-            class="group-header"
+            class="group-header bg-cyan-400 colored-header"
           >
             TOTAL ENDING BALANCE
           </th>
@@ -96,78 +100,105 @@
           <!-- csr -->
           <th
             scope="col"
-            class="header"
+            class="header bg-yellow-400 colored-header"
           >
             QUANTITY
+          </th>
+          <th
+            scope="col"
+            class="header bg-yellow-400 colored-header"
+          >
+            TOTAL COST
           </th>
           <!-- wards -->
           <th
             scope="col"
-            class="header"
+            class="header bg-yellow-400 colored-header"
           >
             QUANTITY
+          </th>
+          <th
+            scope="col"
+            class="header bg-yellow-400 colored-header"
+          >
+            TOTAL COST
           </th>
           <!-- total beginning balance -->
           <th
             scope="col"
-            class="header"
+            class="header bg-yellow-400 colored-header"
           >
             TOTAL QUANTITY
           </th>
           <th
             scope="col"
-            class="header"
+            class="header bg-yellow-400 colored-header"
           >
             TOTAL COST
           </th>
           <!-- supplies issued to wards -->
           <th
             scope="col"
-            class="header"
+            class="header bg-green-400 colored-header"
           >
             TOTAL QUANTITY
           </th>
           <th
             scope="col"
-            class="header"
+            class="header bg-green-400 colored-header"
           >
             TOTAL COST
           </th>
           <!-- CONSUMPTiON -->
           <th
             scope="col"
-            class="header"
+            class="header bg-yellow-600 colored-header"
           >
             QUANTITY
           </th>
           <th
             scope="col"
-            class="header"
+            class="header bg-yellow-600 colored-header"
           >
             TOTAL COST
           </th>
           <!-- TOTAL ENDING BALANCE -->
+          <!-- TOTAL ENDING BALANCE CSR-->
           <th
             scope="col"
-            class="header"
+            class="header bg-cyan-400 colored-header"
           >
             QUANTITY
           </th>
           <th
             scope="col"
-            class="header"
+            class="header bg-cyan-400 colored-header"
+          >
+            TOTAL COST
+          </th>
+          <!-- TOTAL ENDING BALANCE WARD-->
+          <th
+            scope="col"
+            class="header bg-cyan-400 colored-header"
           >
             QUANTITY
           </th>
           <th
             scope="col"
-            class="header"
+            class="header bg-cyan-400 colored-header"
+          >
+            TOTAL COST
+          </th>
+          <!-- TOTAL ENDING BALANCE-->
+          <th
+            scope="col"
+            class="header bg-cyan-400 colored-header"
           >
             TOTAL QUANTITY
           </th>
           <th
             scope="col"
-            class="header"
+            class="header bg-cyan-400 colored-header"
           >
             TOTAL COST
           </th>
@@ -181,7 +212,9 @@
           <td>{{ rc.unit }}</td>
           <td>{{ rc.unit_cost }}</td>
           <td>{{ rc.csr_quantity }}</td>
+          <td>{{ rc.csr_total_cost }}</td>
           <td>{{ rc.ward_quantity }}</td>
+          <td>{{ rc.ward_total_cost }}</td>
           <td>{{ rc.total_beg_total_quantity }}</td>
           <td>{{ rc.total_beg_total_cost }}</td>
           <!-- SUPPLIES ISSUED TO WARDS -->
@@ -237,7 +270,9 @@ export default {
           unit: e.unit,
           unit_cost: e.unit_cost,
           csr_quantity: e.csr_quantity,
+          csr_total_cost: e.csr_total_cost,
           ward_quantity: e.ward_quantity,
+          ward_total_cost: e.ward_total_cost,
           supplies_issued_to_wards_quantity: e.supplies_issued_to_wards_quantity,
           supplies_issued_to_wards_total_cost: e.supplies_issued_to_wards_total_cost,
           total_beg_total_quantity: e.total_beg_total_quantity,
@@ -245,7 +280,9 @@ export default {
           consumption_quantity: e.consumption_quantity,
           consumption_total_cost: e.consumption_total_cost,
           csr_quantity_ending_bal: e.csr_quantity_ending_bal,
+          csr_total_cost_ending_bal: e.csr_total_cost_ending_bal,
           ward_quantity_ending_bal: e.ward_quantity_ending_bal,
+          ward_total_cost_ending_bal: e.ward_total_cost_ending_bal,
           total_end_total_quantity: e.total_end_total_quantity,
           total_end_total_cost: e.total_end_total_cost,
         });
@@ -319,14 +356,17 @@ td {
   border: 1px solid;
   padding: 10px;
 }
+.colored-header {
+  color: black;
+}
 .group-header {
   text-align: center;
-  font-size: 190%;
+  font-size: 120%;
   font-weight: 700;
-  background-color: rgb(0, 156, 120);
+  /* background-color: rgb(0, 156, 120); */
 }
 .header {
-  font-size: 150%;
-  font-weight: 500;
+  font-size: 100%;
+  font-weight: 600;
 }
 </style>
