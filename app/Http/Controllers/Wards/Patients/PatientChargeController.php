ccfa10badbe0ab88de606f3d725bb27f19330f8d
iamnotruzzle <iamnotruzzle@gmail.com>
2023-09-04 09:52:48 +0800

    update query where ward stock only shows when the item is already received

--- app/Http/Controllers/Wards/Patients/PatientChargeController.php
+++ app/Http/Controllers/Wards/Patients/PatientChargeController.php
@@ -37,8 +37,10 @@ class PatientChargeController extends Controller
         // get wards current stocks / MEDICAL SUPPLIES
         $medicalSupplies = DB::table('hclass2')
             ->join('csrw_wards_stocks', 'csrw_wards_stocks.cl2comb', '=', 'hclass2.cl2comb')
+            ->join('csrw_request_stocks', 'csrw_request_stocks.id', '=', 'csrw_wards_stocks.request_stocks_id')
             ->select(DB::raw("hclass2.cl2comb, hclass2.cl2desc, hclass2.uomcode, SUM(csrw_wards_stocks.quantity) as quantity, (SELECT TOP 1 selling_price FROM csrw_item_prices WHERE cl2comb = csrw_wards_stocks.cl2comb ORDER BY created_at DESC) as 'price'"))
             ->where('csrw_wards_stocks.location', $authWardcode->wardcode)
+            ->where('csrw_request_stocks.status', 'RECEIVED')
             ->groupBy('hclass2.cl2comb', 'hclass2.cl2desc', 'hclass2.uomcode', 'csrw_wards_stocks.cl2comb')
             ->get();
 
