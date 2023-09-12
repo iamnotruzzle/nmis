367bb184dbdc3aa13f4a5e838bd243316200992e
iamnotruzzle <iamnotruzzle@gmail.com>
2023-09-12 11:32:16 +0800

    created location stock balance cotroller

---
+++ app/Http/Controllers/LocationStockBalance/LocationStockBalanceController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\LocationStockBalance;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LocationStockBalanceController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
