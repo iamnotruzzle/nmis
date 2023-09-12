40e2bfaa5b4fdcb7dbf3778ae0516aace0a00451
iamnotruzzle <iamnotruzzle@gmail.com>
2023-09-12 14:24:54 +0800

    updated location stock balance controller

--- app/Http/Controllers/LocationStockBalance/LocationStockBalanceController.php
+++ app/Http/Controllers/LocationStockBalance/LocationStockBalanceController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers\LocationStockBalance;
 
 use App\Http\Controllers\Controller;
+use App\Models\WardsStocks;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class LocationStockBalanceController extends Controller
 {
     public function index()
     {
-        //
+        // get auth wardcode
+        $authWardcode = DB::table('csrw_users')
+            ->join('csrw_login_history', 'csrw_users.employeeid', '=', 'csrw_login_history.employeeid')
+            ->select('csrw_login_history.wardcode')
+            ->where('csrw_login_history.employeeid', Auth::user()->employeeid)
+            ->orderBy('csrw_login_history.created_at', 'desc')
+            ->first();
+
+        $currentWardStocks = WardsStocks::with('item_details:cl2comb,cl2desc')
+            ->where('location', $authWardcode->wardcode)
+            ->where('from', 'CSR')
+            ->get();
+
+        return Inertia::render('Wards/Stocks/Index', [
+            // 'items' => $items,
+        ]);
     }
 
     public function store(Request $request)
