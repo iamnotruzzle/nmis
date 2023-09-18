c099d6b0967275628202e6f6bb4c08b6c9c77c2b
iamnotruzzle <iamnotruzzle@gmail.com>
2023-09-18 08:18:47 +0800

    updated filter for the stock balance validation

--- app/Rules/StockBalanceRule.php
+++ app/Rules/StockBalanceRule.php
@@ -5,6 +5,8 @@ namespace App\Rules;
 use App\Models\LocationStockBalance;
 use Carbon\Carbon;
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class StockBalanceRule implements Rule
 {
@@ -27,10 +29,18 @@ class StockBalanceRule implements Rule
      */
     public function passes($attribute, $value)
     {
+        $authWardcode = DB::table('csrw_users')
+            ->join('csrw_login_history', 'csrw_users.employeeid', '=', 'csrw_login_history.employeeid')
+            ->select('csrw_login_history.wardcode')
+            ->where('csrw_login_history.employeeid', Auth::user()->employeeid)
+            ->orderBy('csrw_login_history.created_at', 'desc')
+            ->first();
+
         $date = Carbon::now()->subDays(30); // get last 30 days
 
         $stockBalCount = LocationStockBalance::where('cl2comb', $this->cl2comb)
             ->where('created_at', '>=', $date)
+            ->where('location', $authWardcode->wardcode)
             ->count();
 
         if ($stockBalCount != 0) {
