2add13d4bec3f9e0847dc6a1731d0c919684ab59
iamnotruzzle <iamnotruzzle@gmail.com>
2023-09-26 11:55:42 +0800

    added chart to show completed and pending stock requests

--- app/Http/Controllers/Csr/Dashboard/DashboardController.php
+++ app/Http/Controllers/Csr/Dashboard/DashboardController.php
@@ -18,36 +18,12 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $today = Carbon::today();
-
-        // completed request
-        $completed_request_month = RequestStocks::where('status', 'RECEIVED')
+        // completed requests
+        $completed_requests_month = RequestStocks::where('status', 'RECEIVED')
             ->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->count();
-        $completed_request_week = RequestStocks::where('status', 'RECEIVED')
-            ->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->count();
-        $completed_request_today = RequestStocks::where('status', 'RECEIVED')
-            ->whereDate('created_at', Carbon::now())->count();
-
-        // total cost
-        $total_cost_month = DB::select(
-            "SELECT (SUM(rsd.approved_qty) * (SELECT TOP 1 selling_price FROM csrw_item_prices WHERE cl2comb = rsd.cl2comb ORDER BY created_at DESC)) as total_cost
-            FROM csrw_request_stocks_details  as rsd
-            WHERE rsd.created_at BETWEEN DATEADD(month, DATEDIFF(month, 0, getdate()), 0) AND getdate()
-            GROUP BY rsd.cl2comb"
-        );
-        // for total_cost_week. 0 = first day of week is sunday, 1 = first day of week is monday
-        $total_cost_week = DB::select(
-            "SELECT (SUM(rsd.approved_qty) * (SELECT TOP 1 selling_price FROM csrw_item_prices WHERE cl2comb = rsd.cl2comb ORDER BY created_at DESC)) as total_cost
-            FROM csrw_request_stocks_details  as rsd
-            WHERE DateDiff(wk,getdate(),rsd.created_at) =  0
-            GROUP BY rsd.cl2comb"
-        );
-        $total_cost_today = DB::select(
-            "SELECT (SUM(rsd.approved_qty) * (SELECT TOP 1 selling_price FROM csrw_item_prices WHERE cl2comb = rsd.cl2comb ORDER BY created_at DESC)) as total_cost
-            FROM csrw_request_stocks_details  as rsd
-            WHERE DATEDIFF(d, created_at, GETDATE())=0
-            GROUP BY rsd.cl2comb"
-        );
+        // pending requests
+        $pending_requests_month = RequestStocks::where('status', 'REQUESTED')
+            ->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->count();
 
         // most requested items
         $most_requested_month = DB::select(
@@ -61,12 +37,8 @@ class DashboardController extends Controller
 
 
         return Inertia::render('Csr/Dashboard/Index', [
-            'completed_request_month' => $completed_request_month,
-            'completed_request_week' => $completed_request_week,
-            'completed_request_today' => $completed_request_today,
-            'total_cost_month' => $total_cost_month,
-            'total_cost_week' => $total_cost_week,
-            'total_cost_today' => $total_cost_today,
+            'completed_requests_month' => $completed_requests_month,
+            'pending_requests_month' => $pending_requests_month,
             'most_requested_month' => $most_requested_month,
         ]);
     }
--- resources/js/Pages/Csr/Dashboard/Index.vue
+++ resources/js/Pages/Csr/Dashboard/Index.vue
@@ -2,82 +2,55 @@
   <app-layout>
     <div class="surface-ground">
       <div class="grid">
-        <div class="col-12 md:col-6 lg:col-3">
+        <div class="col-12 md:col-6 lg:col-4">
           <div class="surface-card shadow-2 p-3 border-round">
             <div class="mb-3">
               <div class="flex justify-content-between">
-                <span class="block text-xl text-900 font-bold">Completed request</span>
+                <div class="flex flex-column">
+                  <span class="text-xl text-blue-500 font-semibold">{{ currentMonth }}</span>
+                  <span class="block text-xl text-900 font-bold">Completed requests</span>
+                </div>
+
                 <Link href="issueitems">
                   <div
                     class="flex align-items-center justify-content-center bg-blue-100 border-round"
-                    style="width: 2.5rem; height: 2.5rem"
+                    style="width: 3rem; height: 3rem"
                   >
                     <i class="pi pi-send text-blue-500 text-xl"></i>
                   </div>
                 </Link>
               </div>
             </div>
-            <DataTable
-              :value="completed_request_container"
-              showGridlines
-            >
-              <template #header>
-                <div class="flex justify-content-start">
-                  <p class="text-xl text-blue-500 font-semibold">{{ currentMonth }}</p>
-                </div>
-              </template>
-              <Column
-                field="month"
-                header="MONTHLY"
-              ></Column>
-              <Column
-                field="week"
-                header="WEEKLY"
-              ></Column>
-              <Column
-                field="today"
-                header="TODAY"
-              ></Column>
-            </DataTable>
+            <div class="flex justify-content-center">
+              <span class="text-5xl font-bold text-blue-500">{{ completed_requests_month_container }}</span>
+            </div>
           </div>
         </div>
-        <div class="col-12 md:col-6 lg:col-3">
+        <div class="col-12 md:col-6 lg:col-4">
           <div class="surface-card shadow-2 p-3 border-round">
             <div class="mb-3">
               <div class="flex justify-content-between">
-                <span class="block text-xl text-900 font-bold">Total cost of issued items</span>
+                <div class="flex flex-column">
+                  <span class="text-xl text-yellow-500 font-semibold">{{ currentMonth }}</span>
+                  <span class="block text-xl text-900 font-bold">Pending requests</span>
+                </div>
+
                 <Link href="issueitems">
                   <div
-                    class="flex align-items-center justify-content-center bg-green-100 border-round"
-                    style="width: 2.5rem; height: 2.5rem"
+                    class="flex align-items-center justify-content-center bg-yellow-100 border-round"
+                    style="width: 3rem; height: 3rem"
                   >
-                    <i class="pi pi-money-bill text-green-500 text-xl"></i>
+                    <i class="pi pi-send text-yellow-500 text-xl"></i>
                   </div>
                 </Link>
               </div>
             </div>
-            <DataTable
-              :value="total_cost_container"
-              showGridlines
-            >
-              <template #header>
-                <div class="flex justify-content-start">
-                  <p class="text-xl text-green-500 font-semibold">{{ currentMonth }}</p>
-                </div>
-              </template>
-              <Column header="MONTHLY">
-                <template #body="{ data }"> ₱ {{ data.month }} </template>
-              </Column>
-              <Column header="WEEKLY">
-                <template #body="{ data }"> ₱ {{ data.week }} </template>
-              </Column>
-              <Column header="TODAY">
-                <template #body="{ data }"> ₱ {{ data.today }} </template></Column
-              >
-            </DataTable>
+            <div class="flex justify-content-center">
+              <span class="text-5xl font-bold text-yellow-500">{{ pending_requests_month_container }}</span>
+            </div>
           </div>
         </div>
-        <div class="col-12 md:col-6 lg:col-3">
+        <!-- <div class="col-12 md:col-6 lg:col-3">
           <div class="surface-card shadow-2 p-3 border-round">
             <div class="mb-3">
               <div class="flex justify-content-between">
@@ -112,44 +85,29 @@
               ></Column>
             </DataTable>
           </div>
-        </div>
-        <div class="col-12 md:col-6 lg:col-3">
+        </div> -->
+        <div class="col-12 md:col-6 lg:col-4">
           <div class="surface-card shadow-2 p-3 border-round">
             <div class="mb-3">
               <div class="flex justify-content-between">
-                <span class="block text-xl text-900 font-bold">Completed request</span>
+                <div class="flex flex-column">
+                  <span class="text-xl text-blue-500 font-semibold">{{ currentMonth }}</span>
+                  <span class="block text-xl text-900 font-bold">Completed requests</span>
+                </div>
+
                 <Link href="issueitems">
                   <div
                     class="flex align-items-center justify-content-center bg-blue-100 border-round"
-                    style="width: 2.5rem; height: 2.5rem"
+                    style="width: 3rem; height: 3rem"
                   >
                     <i class="pi pi-send text-blue-500 text-xl"></i>
                   </div>
                 </Link>
               </div>
             </div>
-            <DataTable
-              :value="completed_request_container"
-              showGridlines
-            >
-              <template #header>
-                <div class="flex justify-content-start">
-                  <p class="text-xl text-blue-500 font-semibold">{{ currentMonth }}</p>
-                </div>
-              </template>
-              <Column
-                field="month"
-                header="MONTHLY"
-              ></Column>
-              <Column
-                field="week"
-                header="WEEKLY"
-              ></Column>
-              <Column
-                field="today"
-                header="TODAY"
-              ></Column>
-            </DataTable>
+            <div class="flex justify-content-center">
+              <span class="text-5xl font-bold">10</span>
+            </div>
           </div>
         </div>
       </div>
@@ -172,65 +130,31 @@ export default {
     Column,
   },
   props: {
-    completed_request_month: Number,
-    completed_request_week: Number,
-    completed_request_today: Number,
-    total_cost_month: Object,
-    total_cost_week: Object,
-    total_cost_today: Object,
+    completed_requests_month: Number,
+    pending_requests_month: Number,
     most_requested_month: Object,
-    most_requested_week: Object,
   },
   data() {
     return {
-      completed_request_container: [],
-      total_cost_container: [],
+      completed_requests_month_container: null,
+      pending_requests_month_container: null,
       most_requested_container: [],
       currentMonth: null,
     };
   },
   mounted() {
-    console.log(this.most_requested_month);
-    this.storeValueInRequestContainer();
-    this.storeValueInTotalCostContainer();
-    this.storeValueInMostRequestedContainer();
+    console.log(this.completed_requests_month);
+    this.storeCompletedRequests();
+    this.storePendingRequests();
+    // this.storeValueInMostRequestedContainer();
     this.getCurrentMonth();
   },
   methods: {
-    storeValueInRequestContainer() {
-      this.completed_request_container.push({
-        month: this.completed_request_month,
-        week: this.completed_request_week,
-        today: this.completed_request_today,
-      });
+    storeCompletedRequests() {
+      this.completed_requests_month_container = this.completed_requests_month;
     },
-    storeValueInTotalCostContainer() {
-      // currency formatter
-      const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-      });
-
-      let month = 0;
-      let week = 0;
-      let today = 0;
-
-      this.total_cost_month.forEach((e) => {
-        month += Number(e.total_cost);
-      });
-      this.total_cost_week.forEach((e) => {
-        week += Number(e.total_cost);
-      });
-      this.total_cost_today.forEach((e) => {
-        today += Number(e.total_cost);
-      });
-
-      this.total_cost_container.push({
-        month: formatter.format(month).replace('$', '').trim(),
-        week: formatter.format(week).replace('$', '').trim(),
-        today: formatter.format(today).replace('$', '').trim(),
-      });
+    storePendingRequests() {
+      this.pending_requests_month_container = this.pending_requests_month;
     },
     storeValueInMostRequestedContainer() {
       this.most_requested_month.forEach((e) => {
