781734d49e18de560b62cbdafa09c5617f216f5f
iamnotruzzle <iamnotruzzle@gmail.com>
2023-11-08 14:59:11 +0800

    create patient charge return logws

---
+++ database/migrations/2023_11_08_145048_csrw_patient_charge_return_logs.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CsrwPatientChargeReturnLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csrw_patient_charge_return_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->string('hpercode');
+            $table->string('cl2comb');
+            $table->integer('returned_qty');
+            $table->string('entry_by');
+            $table->string('entry_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csrw_patient_charge_return_logs');
+    }
+}
