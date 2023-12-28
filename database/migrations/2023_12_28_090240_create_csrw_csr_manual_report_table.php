ed718b4f7fc1f665f97480f54595cb12cc69c06b
iamnotruzzle <iamnotruzzle@gmail.com>
2023-12-28 09:18:18 +0800

    created csr maunal report tab;e

---
+++ database/migrations/2023_12_28_090240_create_csrw_csr_manual_report_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCsrwCsrManualReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csrw_csr_manual_report', function (Blueprint $table) {
+            $table->id();
+            $table->string('cl2comb')->nullable();
+            $table->string('uomcode')->nullable();
+            $table->integer('unit_cost')->nullable();
+            $table->integer('csr_beg_bal_quantity')->nullable();
+            $table->integer('csr_beg_bal_total_cost')->nullable();
+            $table->integer('wards_beg_bal_quantity')->nullable();
+            $table->integer('wards_beg_bal_total_cost')->nullable();
+            $table->integer('total_beg_bal_total_quantity')->nullable();
+            $table->integer('total_beg_bal_total_cost')->nullable();
+            $table->integer('supp_issued_to_wards_total_quantity')->nullable();
+            $table->integer('supp_issued_to_wards_total_cost')->nullable();
+            $table->integer('consumption_quantity')->nullable();
+            $table->integer('consumption_total_cost')->nullable();
+            $table->integer('csr_end_bal_quantity)')->nullable();
+            $table->integer('csr_end_bal_total_cost)')->nullable();
+            $table->integer('wards_end_bal_quantity)')->nullable();
+            $table->integer('wards_end_bal_total_cost)')->nullable();
+            $table->integer('total_end_bal_total_quantity)')->nullable();
+            $table->integer('total_end_bal_total_cost)')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csrw_csr_manual_report');
+    }
+}
