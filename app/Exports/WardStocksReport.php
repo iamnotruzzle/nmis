1b20799c714f1be2fd7770c2783c9adb2efd0ae1
iamnotruzzle <iamnotruzzle@gmail.com>
2024-01-04 08:58:00 +0800

    updated excel style

--- app/Exports/WardStocksReport.php
+++ app/Exports/WardStocksReport.php
@@ -5,8 +5,13 @@ namespace App\Exports;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class WardStocksReport implements FromArray, WithHeadings
+class WardStocksReport implements FromArray, WithHeadings, WithStyles, WithEvents
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -16,6 +21,271 @@ class WardStocksReport implements FromArray, WithHeadings
     public function __construct($data)
     {
         $this->data = $data;
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the 1st and 2nd row.
+            1    => ['font' => ['bold' => true, 'size' => 14]],
+            2    => ['font' => ['bold' => true, 'size' => 11]],
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        // dd(count($this->data));
+
+        // $count = count($this->data);
+
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                // 2 == 1st 2 rows = header & sub header
+                $count = count($this->data) + 2;
+
+                // all header
+                $event->sheet->getStyle('A1:P2')->applyFromArray([
+                    'borders' => [
+                        'outline' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ],
+                    ],
+                ]);
+                $event->sheet->getStyle('A1' . ':' . 'P' . $count)->applyFromArray([
+                    'borders' => [
+                        'bottom' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // item description
+                $event->sheet->getStyle('A1' . ':' . 'A' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('A1' . ':' . 'A' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // unit
+                $event->sheet->getStyle('B1' . ':' . 'B' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('B1' . ':' . 'B' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // estimated budget / unit cost
+                $event->sheet->getStyle('C1' . ':' . 'C' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('C1' . ':' . 'C' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('C1:C1')->applyFromArray([
+                    'borders' => [
+                        'bottom' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // beginning balance
+                $event->sheet->getStyle('D1' . ':' . 'D' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('D1' . ':' . 'D' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // received from csr
+                $event->sheet->getStyle('E1' . ':' . 'E' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('E1' . ':' . 'E' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // TOTAL STOCK
+                $event->sheet->getStyle('F1' . ':' . 'F' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('F1' . ':' . 'F' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // consumption
+                $event->sheet->getStyle('G1' . ':' . 'L' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('G1' . ':' . 'L' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('G1:L1')->applyFromArray([
+                    'borders' => [
+                        'bottom' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // surgery
+                $event->sheet->getStyle('G2' . ':' . 'G' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // ob gyne
+                $event->sheet->getStyle('H2' . ':' . 'H' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // ortho
+                $event->sheet->getStyle('I2' . ':' . 'I' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // pedia
+                $event->sheet->getStyle('J2' . ':' . 'J' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // optha
+                $event->sheet->getStyle('K2' . ':' . 'K' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // total consumption
+                $event->sheet->getStyle('M1' . ':' . 'M' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('M1' . ':' . 'M' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // total consumption / estimated cost
+                $event->sheet->getStyle('N1' . ':' . 'N' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('N1' . ':' . 'N' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('N1:N1')->applyFromArray([
+                    'borders' => [
+                        'bottom' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // ending balance
+                $event->sheet->getStyle('O1' . ':' . 'O' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('O1' . ':' . 'O' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                // Actual inventory
+                $event->sheet->getStyle('P1' . ':' . 'P' . $count)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+                $event->sheet->getStyle('P1' . ':' . 'P' . $count)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                        ]
+                    ],
+                ]);
+            },
+        ];
     }
 
     public function headings(): array
