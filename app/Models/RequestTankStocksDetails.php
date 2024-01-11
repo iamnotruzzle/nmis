41aaee1d11393bbabdbd51248da9689218f5e772
iamnotruzzle <iamnotruzzle@gmail.com>
2024-01-11 14:00:43 +0800

    created model for csrw_request_stocks_details table

---
+++ app/Models/RequestTankStocksDetails.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RequestTankStocksDetails extends Model
+{
+    use HasFactory;
+
+    protected $table = 'csrw_request_stocks_details';
+
+    protected $fillable = [
+        'id',
+        'request_stocks_id',
+        'itemcode',
+        'requested_qty',
+        'approved_qty',
+        'remarks',
+    ];
+
+    public function request_stocks()
+    {
+        return $this->hasOne(RequestTankStocks::class, 'request_stocks_id', 'id');
+    }
+
+    // public function item_details()
+    // {
+    //     return $this->hasOne(Item::class, 'cl2comb', 'cl2comb');
+    // }
+
+    // public function stocks()
+    // {
+    //     return $this->hasMany(CsrStocksMedicalSupplies::class, 'cl2comb', 'cl2comb')->with(['brandDetail:id,name', 'ward_stocks'])
+    //         ->whereDate('expiration_date', '>', Carbon::today());
+    // }
+}
