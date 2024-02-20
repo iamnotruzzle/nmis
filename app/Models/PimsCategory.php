acaba5e76268f52527981ddd690eadab62566a3e
iamnotruzzle <iamnotruzzle@gmail.com>
2024-02-20 08:48:41 +0800

    created pims migration and model

---
+++ app/Models/PimsCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PimsCategory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'csrw_pims_category'; // list of proc type for hclass tables
+
+    protected $fillable = [
+        'id',
+        'name',
+        'status',
+    ];
+}
---
+++ database/migrations/2024_02_20_084602_create_csrw_pims_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCsrwPimsCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csrw_pims_category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('status')->default('N');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csrw_pims_category');
+    }
+}
