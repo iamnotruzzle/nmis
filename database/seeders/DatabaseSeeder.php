73d55cd0f2263196e0aa675855941f21688e8b2a
iamnotruzzle <iamnotruzzle@gmail.com>
2024-02-20 16:20:30 +0800

    breakpoint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RoleAndPermissionSeeder::class,
             BrandSeeder::class,
             FundSourceSeeder::class,
+            PimsCategorySeeder::class
         ]);
     }
 }
