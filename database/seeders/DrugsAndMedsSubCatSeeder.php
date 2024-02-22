e1a82a23ee887dc70f8d43badcdc079a13e15733
iamnotruzzle <iamnotruzzle@gmail.com>
2024-02-22 10:39:29 +0800

    created all the seeder and data of sub categories

---
+++ database/seeders/DrugsAndMedsSubCatSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Support\Facades\File;
+use Illuminate\Database\Seeder;
+
+class DrugsAndMedsSubCatSeeder extends Seeder
+{
+    public function run()
+    {
+
+        $json = File::get('database/data/it_supplies_sub_category.json');
+        $data = json_decode($json);
+
+        foreach ($data as $obj) {
+            $subCategory = Category::firstOrCreate([
+                'cl1comb' => '1000' . '-' . 'p' . $obj->itemid,
+                'ptcode' => '1000',
+                'cl1code' => 'p' . $obj->itemid,
+                'cl1desc' => $obj->item,
+                'cl1stat' => 'A',
+                'cl1lock' => 'N',
+                'cl1upsw' => 'P',
+                'cl1dtmd' => NULL,
+                'compense' => NULL,
+                'catID' => $obj->catID,
+            ]);
+        }
+    }
+}
