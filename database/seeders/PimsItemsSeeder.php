d53a33ec1db8fa79ddf6054b88d10240b7a99c58
iamnotruzzle <iamnotruzzle@gmail.com>
2024-04-02 13:56:57 +0800

    breakpoint

--- database/seeders/PimsItemsSeeder.php
+++ database/seeders/PimsItemsSeeder.php
@@ -3,11 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Item;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class PimsItemsSeeder extends Seeder
 {
+    public function generateUniqueID($length = 7)
+    {
+        return Str::random($length);
+    }
+
     public function run()
     {
         // $categories = item column is null, ' ' or '.', the item column will be equal to its description column
@@ -32,12 +40,12 @@ class PimsItemsSeeder extends Seeder
                 $seenItems[$hash] = true;
             }
         }
-        // dd($duplicateItemsRemoved);
 
+        // saving category to table
         foreach ($duplicatedItemsRemoved as $obj) {
             $trimmedItem = trim($obj->item);
 
-            $subCategory = Category::firstOrCreate([
+            $category = Category::firstOrCreate([
                 'cl1comb' => '1000' . '-' . $obj->itemid,
                 'ptcode' => '1000',
                 'cl1code' => $obj->itemid,
@@ -50,5 +58,44 @@ class PimsItemsSeeder extends Seeder
                 'catID' => $obj->catid,
             ]);
         }
+
+        // saving items to table
+        // foreach ($data as $obj) {
+        //     $uniqueID = $this->generateUniqueID();
+
+        //     $item = Item::firstOrCreate(
+        //         [
+        //             // 1000 = ptcode from hclass1
+        //             // 'p' . $obj->cl1code = cl1code from hclass1
+        //             'cl2comb' => '1000' . '-' . cl1code from category . '-' . $uniqueID,
+        //             'cl1comb' => '1000' . '-' . cl1code from category,
+        //             'cl2code' => $uniqueID,
+        //             'stkno' => '',
+        //             'cl2desc' => $obj->description,
+        //             'cl2retprc' => 0.00,
+        //             'uomcode' => 'PC',
+        //             'cl2dteas' => Carbon::now(),
+        //             'cl2stat' => 'I',
+        //             'cl2lock' => 'N',
+        //             'cl2upsw' => 'P',
+        //             'cl2dtmd' => NULL,
+        //             'curcode' => NULL,
+        //             'cl2purp' => NULL,
+        //             'curcode1' => NULL,
+        //             'uomcode1' => NULL,
+        //             'cl2ctr' => NULL,
+        //             'brandname' => NULL,
+        //             'stockbal' => 0.00,
+        //             'pharmaceutical' => NULL,
+        //             'pharmaceutical' => NULL,
+        //             'baldteasof' => Carbon::now(),
+        //             'begbal' => 0.00,
+        //             'lot_no' => '',
+        //             'barcode' => NULL,
+        //             'rpoint' => NULL,
+        //             'catID' => $obj->catID
+        //         ]
+        //     );
+        // }
     }
 }
