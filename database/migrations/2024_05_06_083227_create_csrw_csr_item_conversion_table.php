62269f6648e3decb6f0772c272662af00cba414e
iamnotruzzle <iamnotruzzle@gmail.com>
2024-05-06 08:38:17 +0800

    created migration for item conversion

---
+++ database/migrations/2024_05_06_083227_create_csrw_csr_item_conversion_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCsrwCsrItemConversionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csrw_csr_item_conversion', function (Blueprint $table) {
+            $table->id();
+            $table->string('csr_stock_id');
+            $table->string('ris_no');
+            $table->string('cl2comb_before');
+            $table->string('uomcode_before');
+            $table->integer('quantity_before');
+            $table->string('cl2comb_after');
+            $table->string('uomcode_after');
+            $table->integer('quantity_after');
+            $table->bigInteger('brand')->nullable();
+            $table->string('suppcode')->nullable();
+            $table->dateTime('manufactured_date')->nullable();
+            $table->dateTime('delivered_date')->nullable();
+            $table->dateTime('expiration_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csrw_csr_item_conversion');
+    }
+}
