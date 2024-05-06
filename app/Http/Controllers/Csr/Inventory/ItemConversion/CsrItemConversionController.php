9659128979b313ae5d56343473a670a286110abb
iamnotruzzle <iamnotruzzle@gmail.com>
2024-05-06 08:46:23 +0800

    created controller for csr item conversion

---
+++ app/Http/Controllers/Csr/Inventory/ItemConversion/CsrItemConversionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Csr\Inventory\ItemConversion;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CsrItemConversionController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
