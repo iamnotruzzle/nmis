ac8142815cef578ac4aed973c7d8a544fe03ce68
iamnotruzzle <iamnotruzzle@gmail.com>
2024-05-16 11:27:38 +0800

    breakpoint

--- resources/js/Pages/Wards/RequestStocks/Index.vue
+++ resources/js/Pages/Wards/RequestStocks/Index.vue
@@ -1585,7 +1585,7 @@ export default {
     },
     'formConsignment.cl2comb': function (val) {
       this.selectedItemsUomDesc = null;
-      console.log(this.itemsList);
+      //   console.log(this.itemsList);
 
       this.itemsList.forEach((e) => {
         if (e.cl2comb == val) {
