0135dee2006972b1996a80b06a4fbb633ab4544d
iamnotruzzle <iamnotruzzle@gmail.com>
2024-08-30 09:39:15 +0800

    created RETURNED ITEMS viewer

--- app/Http/Controllers/Csr/ReturnedItems/ReturnedItemsController.php
+++ app/Http/Controllers/Csr/ReturnedItems/ReturnedItemsController.php
@@ -4,12 +4,38 @@ namespace App\Http\Controllers\Csr\ReturnedItems;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class ReturnedItemsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $result = DB::select(
+            "SELECT returned_items.id, returned_items.ris_no, item.cl2desc as item, returned_items.quantity, ward.wardcode, ward.wardname as ward, employee.firstname, employee.lastname, returned_items.remarks, returned_items.created_at
+                FROM csrw_csr_returned_items as returned_items
+                JOIN hclass2 as item ON item.cl2comb = returned_items.cl2comb
+                JOIN hward as ward ON ward.wardcode = returned_items.[from]
+                JOIN hpersonal as employee ON employee.employeeid = returned_items.returned_by;"
+        );
+
+        foreach ($result as $r) {
+            $returnedItems[] = (object) [
+                'id' => $r->id,
+                'ris_no' => $r->ris_no,
+                'item' => $r->item,
+                'quantity' => $r->quantity,
+                'wardcode' => $r->wardcode,
+                'ward' => $r->ward,
+                'returned_by' => $r->firstname . ' ' . $r->lastname,
+                'remarks' => $r->remarks,
+                'created_at' => $r->created_at,
+            ];
+        }
+
+        return Inertia::render('Csr/ReturnedItems/Index', [
+            'returnedItems' => $returnedItems,
+        ]);
     }
 
     public function store(Request $request)
---
+++ resources/js/Pages/Csr/ReturnedItems/Index.vue
@@ -0,0 +1,240 @@
+<template>
+  <app-layout>
+    <Head title="NMIS - Returned Items" />
+
+    <div
+      class="card"
+      style="width: 100%"
+    >
+      <Toast />
+
+      <DataTable
+        class="p-datatable-sm"
+        v-model:filters="filters"
+        :value="returnedItemsList"
+        paginator
+        :rows="20"
+        :rowsPerPageOptions="[20, 30, 40]"
+        dataKey="id"
+        filterDisplay="row"
+        sortField="created_at"
+        :sortOrder="1"
+        removableSort
+        :globalFilterFields="['ward', 'ris_no', 'item']"
+        showGridlines
+      >
+        <template #header>
+          <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+            <span class="text-xl text-900 font-bold text-primary">RETURNED ITEMS</span>
+            <div class="flex">
+              <div class="mr-2">
+                <div class="p-inputgroup">
+                  <span class="p-inputgroup-addon">
+                    <i class="pi pi-search"></i>
+                  </span>
+                  <InputText
+                    id="searchInput"
+                    v-model="filters['global'].value"
+                    placeholder="Search"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+        <template #empty> No data found. </template>
+        <template #loading> Loading data. Please wait. </template>
+        <!-- <Column
+          field="cl1comb"
+          header="ID"
+          style="width: 5%"
+        >
+        </Column> -->
+        <Column
+          field="ward"
+          header="FROM"
+          sortable
+          :showFilterMenu="false"
+        >
+          <template #body="{ data }">
+            {{ data.ward }}
+          </template>
+
+          <template #filter="{ filterModel, filterCallback }">
+            <Dropdown
+              v-model="filterModel.value"
+              :options="locationFilter"
+              @change="filterCallback()"
+              :virtualScrollerOptions="{ itemSize: 38 }"
+              filter
+              optionLabel="name"
+              optionValue="name"
+              placeholder="NO FILTER"
+            />
+          </template>
+        </Column>
+        <Column
+          field="ris_no"
+          header="RIS NO."
+        >
+        </Column>
+        <Column
+          field="item"
+          header="ITEM"
+          sortable
+        >
+        </Column>
+        <!-- breakpoint -->
+        <Column
+          field="quantity"
+          header="QTY"
+          sortable
+          style="width: 5%"
+        >
+          <template #body="{ data }">
+            <p class="text-right">
+              {{ data.quantity }}
+            </p>
+          </template>
+        </Column>
+        <Column
+          field="returned_by"
+          header="RETURNED BY"
+          sortable
+        >
+        </Column>
+        <Column
+          field="remarks"
+          header="REMARKS"
+        >
+        </Column>
+        <Column
+          field="created_at"
+          header="RETURN DATE"
+          :showFilterMenu="false"
+          sortable
+        >
+          <template #body="{ data }">
+            {{ tzone(data.created_at) }}
+          </template>
+        </Column>
+      </DataTable>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import { FilterMatchMode } from 'primevue/api';
+import { router } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import FileUpload from 'primevue/fileupload';
+import Toast from 'primevue/toast';
+import Avatar from 'primevue/avatar';
+import Calendar from 'primevue/calendar';
+import Dropdown from 'primevue/dropdown';
+import AutoComplete from 'primevue/autocomplete';
+import IconField from 'primevue/iconField';
+import Tag from 'primevue/tag';
+import moment from 'moment';
+import { Link } from '@inertiajs/vue3';
+
+export default {
+  components: {
+    AppLayout,
+    Head,
+    InputText,
+    Column,
+    Password,
+    DataTable,
+    Button,
+    Dialog,
+    FileUpload,
+    Toast,
+    Avatar,
+    Calendar,
+    Dropdown,
+    AutoComplete,
+    Tag,
+    Link,
+    IconField,
+  },
+  props: {
+    returnedItems: Object,
+  },
+  data() {
+    return {
+      //   categoryFilter: [
+      //     { name: 'Accountable forms', catID: 22 },
+      //   ],
+      returnedItemsList: [],
+      locationFilter: [],
+      filters: {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        ward: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        ris_no: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        item: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      },
+    };
+  },
+  mounted() {
+    this.storeWardsInventoryInContainer();
+    this.storeLocationsInContainer();
+  },
+  methods: {
+    storeWardsInventoryInContainer() {
+      this.returnedItemsList = []; // reset
+
+      this.returnedItems.forEach((e) => {
+        this.returnedItemsList.push({
+          id: e.id,
+          ris_no: e.ris_no,
+          wardcode: e.wardcode,
+          ward: e.ward,
+          item: e.item,
+          quantity: e.quantity,
+          returned_by: e.returned_by,
+          remarks: e.remarks,
+          created_at: e.created_at,
+        });
+      });
+    },
+    storeLocationsInContainer() {
+      this.$page.props.locations.forEach((e) => {
+        if (e.wardcode != 'CSR' && e.wardcode != 'ADMIN') {
+          this.locationFilter.push({
+            code: e.wardcode,
+            name: e.wardname,
+          });
+        }
+      });
+
+      //   console.log(this.locationsList);
+    },
+    tzone(date) {
+      return moment.tz(date, 'Asia/Manila').format('L');
+    },
+  },
+};
+</script>
+<style scoped>
+/* Remove arrow for input type number */
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+/* END Remove arrow for input type number */
+</style>
