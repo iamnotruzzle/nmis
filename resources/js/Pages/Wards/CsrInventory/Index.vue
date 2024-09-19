f4530a580ab2068833b35993cb7cb11212448b8e
iamnotruzzle <iamnotruzzle@gmail.com>
2024-09-19 11:54:10 +0800

    update design

--- resources/js/Pages/Wards/CsrInventory/Index.vue
+++ resources/js/Pages/Wards/CsrInventory/Index.vue
@@ -2,139 +2,149 @@
   <app-layout>
     <Head title="NMIS - Wards Inventory" />
 
+    <Toast />
+
     <div
-      class="card flex flex-row justify-content-around"
+      class="flex flex-row justify-content-around"
       style="width: 100%"
     >
-      <Toast />
-
-      <DataTable
-        class="p-datatable-sm w-full"
-        v-model:filters="filters"
-        :value="csrInventoryList"
-        paginator
-        :rows="20"
-        :rowsPerPageOptions="[20, 30, 40]"
-        dataKey="id"
-        sortField="item_desc"
-        :sortOrder="1"
-        removableSort
-        :globalFilterFields="['item_desc']"
-        showGridlines
+      <div
+        class="card"
+        style="width: 100%"
       >
-        <template #header>
-          <div class="flex flex-wrap align-items-center justify-content-between gap-2">
-            <span class="text-xl text-900 font-bold text-primary">CSR INVENTORY</span>
-            <div class="flex">
-              <div class="mr-2">
-                <div class="p-inputgroup">
-                  <span class="p-inputgroup-addon">
-                    <i class="pi pi-search"></i>
-                  </span>
-                  <InputText
-                    id="searchInput"
-                    v-model="filters['global'].value"
-                    placeholder="Search"
-                  />
+        <DataTable
+          class="p-datatable-sm w-full"
+          v-model:filters="filters"
+          :value="csrInventoryList"
+          paginator
+          :rows="20"
+          :rowsPerPageOptions="[20, 30, 40]"
+          dataKey="id"
+          sortField="item_desc"
+          :sortOrder="1"
+          removableSort
+          :globalFilterFields="['item_desc']"
+          showGridlines
+        >
+          <template #header>
+            <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+              <span class="text-xl text-900 font-bold text-primary">CSR INVENTORY</span>
+              <div class="flex">
+                <div class="mr-2">
+                  <div class="p-inputgroup">
+                    <span class="p-inputgroup-addon">
+                      <i class="pi pi-search"></i>
+                    </span>
+                    <InputText
+                      id="searchInput"
+                      v-model="filters['global'].value"
+                      placeholder="Search"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </template>
-        <template #empty> No data found. </template>
-        <template #loading> Loading data. Please wait. </template>
-        <!-- <Column
+          </template>
+          <template #empty> No data found. </template>
+          <template #loading> Loading data. Please wait. </template>
+          <!-- <Column
           field="cl1comb"
           header="ID"
           style="width: 5%"
         >
         </Column> -->
-        <Column
-          field="item_desc"
-          header="ITEM"
-          sortable
-        >
-        </Column>
-        <!-- breakpoint -->
-        <Column
-          field="quantity"
-          header="QUANTITY"
-          sortable
-          style="width: 5%; text-align: right"
-          :pt="{ headerContent: 'justify-content-end' }"
-        >
-          <template #body="{ data }">
-            <p class="text-right">
-              {{ data.quantity }}
-            </p>
-          </template>
-        </Column>
-      </DataTable>
+          <Column
+            field="item_desc"
+            header="ITEM"
+            sortable
+          >
+          </Column>
+          <!-- breakpoint -->
+          <Column
+            field="quantity"
+            header="QUANTITY"
+            sortable
+            style="width: 5%; text-align: right"
+            :pt="{ headerContent: 'justify-content-end' }"
+          >
+            <template #body="{ data }">
+              <p class="text-right">
+                {{ data.quantity }}
+              </p>
+            </template>
+          </Column>
+        </DataTable>
+      </div>
 
       <div class="mx-2"></div>
 
-      <DataTable
-        class="p-datatable-sm w-full"
-        v-model:filters="filtersCurrentStock"
-        :value="currentStockList"
-        paginator
-        :rows="20"
-        :rowsPerPageOptions="[20, 30, 40]"
-        dataKey="id"
-        sortField="item_desc"
-        :sortOrder="1"
-        removableSort
-        :globalFilterFields="['item_desc']"
-        showGridlines
+      <div
+        class="card"
+        style="width: 100%"
       >
-        <template #header>
-          <div class="flex flex-wrap align-items-center justify-content-between gap-2">
-            <span class="text-xl text-900 font-bold text-green-500">CURRENT STOCK</span>
-            <div class="flex">
-              <div class="mr-2">
-                <div class="p-inputgroup">
-                  <span class="p-inputgroup-addon">
-                    <i class="pi pi-search"></i>
-                  </span>
-                  <InputText
-                    id="searchInput"
-                    v-model="filtersCurrentStock['global'].value"
-                    placeholder="Search"
-                  />
+        <DataTable
+          class="p-datatable-sm w-full"
+          v-model:filters="filtersCurrentStock"
+          :value="currentStockList"
+          paginator
+          :rows="20"
+          :rowsPerPageOptions="[20, 30, 40]"
+          dataKey="id"
+          sortField="item_desc"
+          :sortOrder="1"
+          removableSort
+          :globalFilterFields="['item_desc']"
+          showGridlines
+        >
+          <template #header>
+            <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+              <span class="text-xl text-900 font-bold text-green-500">CURRENT STOCK</span>
+              <div class="flex">
+                <div class="mr-2">
+                  <div class="p-inputgroup">
+                    <span class="p-inputgroup-addon">
+                      <i class="pi pi-search"></i>
+                    </span>
+                    <InputText
+                      id="searchInput"
+                      v-model="filtersCurrentStock['global'].value"
+                      placeholder="Search"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </template>
-        <template #empty> No data found. </template>
-        <template #loading> Loading data. Please wait. </template>
-        <!-- <Column
+          </template>
+          <template #empty> No data found. </template>
+          <template #loading> Loading data. Please wait. </template>
+          <!-- <Column
           field="cl1comb"
           header="ID"
           style="width: 5%"
         >
         </Column> -->
-        <Column
-          field="item_desc"
-          header="ITEM"
-          sortable
-        >
-        </Column>
-        <!-- breakpoint -->
-        <Column
-          field="quantity"
-          header="QUANTITY"
-          sortable
-          style="width: 5%; text-align: right"
-          :pt="{ headerContent: 'justify-content-end' }"
-        >
-          <!-- <template #body="{ data }">
+          <Column
+            field="item_desc"
+            header="ITEM"
+            sortable
+          >
+          </Column>
+          <!-- breakpoint -->
+          <Column
+            field="quantity"
+            header="QUANTITY"
+            sortable
+            style="width: 5%; text-align: right"
+            :pt="{ headerContent: 'justify-content-end' }"
+          >
+            <!-- <template #body="{ data }">
               <p class="text-right">
                 {{ data.quantity }}
               </p>
             </template> -->
-        </Column>
-      </DataTable>
+          </Column>
+        </DataTable>
+      </div>
     </div>
   </app-layout>
 </template>
