a4b40187dd20716e28e0d37835b3d43d8bb0999b
iamnotruzzle <iamnotruzzle@gmail.com>
2024-11-15 11:24:51 +0800

    update csr query

--- app/Http/Controllers/Csr/Reports/ReportsController.php
+++ app/Http/Controllers/Csr/Reports/ReportsController.php
@@ -51,8 +51,8 @@ class ReportsController extends Controller
         $ward_report = DB::select(
             "SELECT
                 ward.stock_id AS csr_stock_id,
-                SUM(csrw_location_stock_balance.beginning_balance) AS beginning_balance,
-                SUM(csrw_location_stock_balance.ending_balance) AS ending_balance
+                SUM(csrw_location_stock_balance.beginning_balance) AS ward_beginning_balance,
+                SUM(csrw_location_stock_balance.ending_balance) AS ward_ending_balance
             FROM
                 csrw_wards_stocks AS ward
             JOIN hclass2 ON ward.cl2comb = hclass2.cl2comb
@@ -102,8 +102,8 @@ class ReportsController extends Controller
             $combinedResults[$row->csr_stock_id] = array_merge(
                 (array)$row,
                 [
-                    'beginning_balance' => $row->beginning_balance ?? 0,
-                    'ending_balance' => $row->ending_balance ?? 0
+                    'ward_beginning_balance' => $row->ward_beginning_balance ?? 0,
+                    'ward_ending_balance' => $row->ward_ending_balance ?? 0
                 ]
             );
         }
@@ -121,8 +121,8 @@ class ReportsController extends Controller
                 $combinedResults[$csr_stock_id] = array_merge(
                     (array)$row,
                     [
-                        'beginning_balance' => 0,
-                        'ending_balance' => 0
+                        'ward_beginning_balance' => 0,
+                        'ward_ending_balance' => 0
                     ]
                 );
             }
@@ -147,8 +147,8 @@ class ReportsController extends Controller
                     'issued_total_cost' => $record['issued_total_cost'] ?? 0,
                     'consump_quantity' => $record['consump_quantity'] ?? 0,
                     'consump_total_cost' => $record['consump_total_cost'] ?? 0,
-                    'beginning_balance' => $record['beginning_balance'],
-                    'ending_balance' => $record['ending_balance'],
+                    'ward_beginning_balance' => $record['ward_beginning_balance'],
+                    'ward_ending_balance' => $record['ward_ending_balance'],
                 ];
             } else {
                 // Sum the values for the existing entry
@@ -159,134 +159,11 @@ class ReportsController extends Controller
                 $aggregatedResults[$key]['issued_total_cost'] += $record['issued_total_cost'] ?? 0;
                 $aggregatedResults[$key]['consump_quantity'] += $record['consump_quantity'] ?? 0;
                 $aggregatedResults[$key]['consump_total_cost'] += $record['consump_total_cost'] ?? 0;
-                $aggregatedResults[$key]['beginning_balance'] += $record['beginning_balance'];
-                $aggregatedResults[$key]['ending_balance'] += $record['ending_balance'];
+                $aggregatedResults[$key]['ward_beginning_balance'] += $record['ward_beginning_balance'];
+                $aggregatedResults[$key]['ward_ending_balance'] += $record['ward_ending_balance'];
             }
         }
         dd($aggregatedResults);
-
-        // if (is_null($request->from) || is_null($request->to)) {
-        //     // WHERE created_at BETWEEN DATEADD(month, DATEDIFF(month, 0, getdate()), 0) AND getdate()
-        //     // the where above means between the first day and last day of this month
-        //     $csr_report = DB::select(
-        //         "SELECT hclass2.cl2comb,
-        //         hclass2.cl2desc,
-        //         huom.uomdesc,
-        //         clsb_csr.beginning_balance as csr_beginning_balance,
-        //         clsb_csr.ending_balance as csr_ending_balance,
-        //         clsb_ward.beginning_balance as ward_beginning_balance,
-        //         clsb_ward.ending_balance as ward_ending_balance,
-        //         (SELECT TOP 1 price_per_unit FROM csrw_item_prices WHERE cl2comb = hclass2.cl2comb ORDER BY created_at DESC) as 'price_per_unit',
-        //         SUM(csrw_csr_stocks.quantity) as csr_quantity,
-        //         csrw_wards_stocks.wards_quantity,
-        //         csrw_wards_stocks.converted_quantity as converted_quantity,
-        //         csrw_patient_charge_logs.charge_quantity as consumption_quantity,
-        //         csrw_patient_charge_logs.charge_total as consumption_total_cost
-        //         FROM csrw_csr_stocks
-        //         JOIN hclass2 ON csrw_csr_stocks.cl2comb = hclass2.cl2comb
-        //         LEFT JOIN (
-        //             SELECT ward.cl2comb, SUM(ward.quantity) as wards_quantity, SUM(ward.converted_quantity) as converted_quantity
-        //             FROM csrw_wards_stocks as ward
-        //             WHERE ward.[from] = 'CSR'
-        //             GROUP BY ward.cl2comb
-        //         ) csrw_wards_stocks ON hclass2.cl2comb = csrw_wards_stocks.cl2comb
-        //         LEFT JOIN (
-        //             SELECT charge.itemcode, SUM(charge.quantity) as charge_quantity, SUM(charge.price_total) as charge_total
-        //             FROM csrw_patient_charge_logs as charge
-        //             WHERE charge.[from] = 'CSR'
-        //             GROUP BY charge.itemcode
-        //         ) csrw_patient_charge_logs ON csrw_csr_stocks.cl2comb = csrw_patient_charge_logs.itemcode
-        //         LEFT JOIN huom ON csrw_csr_stocks.uomcode = huom.uomcode
-        //         RIGHT JOIN (
-        //             SELECT id, cl2comb, ending_balance, beginning_balance
-        //             FROM csrw_location_stock_balance
-        //             WHERE location = 'CSR'
-        //         ) AS clsb_csr ON hclass2.cl2comb = clsb_csr.cl2comb
-        //         LEFT JOIN (
-        //             SELECT cl2comb, SUM(ending_balance) as ending_balance, SUM(beginning_balance) as beginning_balance
-        //             FROM csrw_location_stock_balance
-        //             WHERE location != 'CSR'
-        //             GROUP BY cl2comb
-        //         ) AS clsb_ward ON hclass2.cl2comb = clsb_ward.cl2comb
-        //         WHERE created_at BETWEEN DATEADD(month, DATEDIFF(month, 0, getdate()), 0) AND getdate()
-        //         GROUP BY hclass2.cl2comb, hclass2.cl2desc, huom.uomdesc, csrw_wards_stocks.wards_quantity, csrw_patient_charge_logs.charge_quantity, csrw_patient_charge_logs.charge_total,
-        //         clsb_csr.beginning_balance, clsb_csr.ending_balance, clsb_ward.beginning_balance, clsb_ward.ending_balance, csrw_wards_stocks.converted_quantity
-        //         ORDER BY hclass2.cl2desc ASC;"
-        //     );
-        //     // dd($csr_report);
-        // } else {
-        //     $csr_report = DB::select(
-        //         "SELECT hclass2.cl2comb,
-        //         hclass2.cl2desc,
-        //         huom.uomdesc,
-        //         clsb_csr.beginning_balance as csr_beginning_balance,
-        //         clsb_csr.ending_balance as csr_ending_balance,
-        //         clsb_ward.beginning_balance as ward_beginning_balance,
-        //         clsb_ward.ending_balance as ward_ending_balance,
-        //         (SELECT TOP 1 price_per_unit FROM csrw_item_prices WHERE cl2comb = hclass2.cl2comb ORDER BY created_at DESC) as 'price_per_unit',
-        //         SUM(csrw_csr_stocks.quantity) as csr_quantity,
-        //         csrw_wards_stocks.wards_quantity,
-        //         csrw_wards_stocks.converted_quantity as converted_quantity,
-        //         csrw_patient_charge_logs.charge_quantity as consumption_quantity,
-        //         csrw_patient_charge_logs.charge_total as consumption_total_cost
-        //         FROM csrw_csr_stocks
-        //         JOIN hclass2 ON csrw_csr_stocks.cl2comb = hclass2.cl2comb
-        //         LEFT JOIN (
-        //             SELECT ward.cl2comb, SUM(ward.quantity) as wards_quantity, SUM(ward.converted_quantity) as converted_quantity
-        //             FROM csrw_wards_stocks as ward
-        //             WHERE ward.[from] = 'CSR'
-        //             GROUP BY ward.cl2comb
-        //         ) csrw_wards_stocks ON hclass2.cl2comb = csrw_wards_stocks.cl2comb
-        //         LEFT JOIN (
-        //             SELECT charge.itemcode, SUM(charge.quantity) as charge_quantity, SUM(charge.price_total) as charge_total
-        //             FROM csrw_patient_charge_logs as charge
-        //             WHERE charge.[from] = 'CSR'
-        //             GROUP BY charge.itemcode
-        //         ) csrw_patient_charge_logs ON csrw_csr_stocks.cl2comb = csrw_patient_charge_logs.itemcode
-        //         LEFT JOIN huom ON csrw_csr_stocks.uomcode = huom.uomcode
-        //         RIGHT JOIN (
-        //             SELECT id, cl2comb, ending_balance, beginning_balance
-        //             FROM csrw_location_stock_balance
-        //             WHERE location = 'CSR'
-        //         ) AS clsb_csr ON hclass2.cl2comb = clsb_csr.cl2comb
-        //         LEFT JOIN (
-        //             SELECT cl2comb, SUM(ending_balance) as ending_balance, SUM(beginning_balance) as beginning_balance
-        //             FROM csrw_location_stock_balance
-        //             WHERE location != 'CSR'
-        //             GROUP BY cl2comb
-        //         ) AS clsb_ward ON hclass2.cl2comb = clsb_ward.cl2comb
-        //         WHERE created_at BETWEEN '$from' AND '$to'
-        //         GROUP BY hclass2.cl2comb, hclass2.cl2desc, huom.uomdesc, csrw_wards_stocks.wards_quantity, csrw_patient_charge_logs.charge_quantity, csrw_patient_charge_logs.charge_total,
-        //         clsb_csr.beginning_balance, clsb_csr.ending_balance, clsb_ward.beginning_balance, clsb_ward.ending_balance, csrw_wards_stocks.converted_quantity
-        //         ORDER BY hclass2.cl2desc ASC;"
-        //     );
-        // }
-        // // dd($csr_report);
-
-        // foreach ($csr_report as $e) {
-        //     $reports[] = (object) [
-        //         'item_description' => $e->cl2desc,
-        //         'unit' => $e->uomdesc,
-        //         'unit_cost' => $e->price_per_unit,
-        //         'csr_quantity' => $e->csr_beginning_balance, // csr starting balance
-        //         'csr_total_cost' => $e->csr_beginning_balance * $e->price_per_unit,
-        //         'ward_quantity' => $e->ward_beginning_balance, // ward starting balance
-        //         'ward_total_cost' => $e->ward_beginning_balance * $e->price_per_unit,
-        //         'total_beg_total_quantity' => $e->csr_beginning_balance + $e->ward_beginning_balance,
-        //         'total_beg_total_cost' => ($e->csr_beginning_balance + $e->ward_beginning_balance) * $e->price_per_unit,
-        //         'supplies_issued_to_wards_quantity' => $e->wards_quantity + $e->consumption_quantity + $e->converted_quantity, // + converted quantity
-        //         'supplies_issued_to_wards_total_cost' => ($e->wards_quantity + $e->consumption_quantity) * $e->price_per_unit,
-        //         'consumption_quantity' => $e->consumption_quantity,
-        //         'consumption_total_cost' => $e->consumption_total_cost,
-        //         'csr_quantity_ending_bal' => $e->csr_ending_balance, // csr ending balance
-        //         'csr_total_cost_ending_bal' => $e->csr_ending_balance * $e->price_per_unit,
-        //         'ward_quantity_ending_bal' => $e->ward_ending_balance,
-        //         'ward_total_cost_ending_bal' => $e->ward_ending_balance * $e->price_per_unit, // ward ending balance
-        //         'total_end_total_quantity' => $e->csr_ending_balance + $e->ward_ending_balance,
-        //         'total_end_total_cost' => ($e->csr_ending_balance + $e->ward_ending_balance) * $e->price_per_unit,
-        //     ];
-        // }
-        // dd($reports);
 
         return Inertia::render('Csr/Reports/Index', [
             // 'reports' => $reports
