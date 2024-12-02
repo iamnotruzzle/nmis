18381b17d45c61e5c5947c4ecbf7234070f17089
iamnotruzzle <iamnotruzzle@gmail.com>
2024-12-02 16:22:06 +0800

    update

--- resources/js/Pages/Wards/Faq/Index.vue
+++ resources/js/Pages/Wards/Faq/Index.vue
@@ -3,9 +3,12 @@
     <Head title="NMIS - Faq" />
 
     <div class="card">
+      <h3>FREQUENTLY ASKED QUESTIONS</h3>
+
       <Fieldset
-        legend="Adding stocks to inventory"
+        legend="ADDING STOCKS TO INVENTORY"
         :toggleable="true"
+        collapsed
       >
         <p class="m-0">
           1. Go to the sidebar and click on <span class="font-bold text-yellow-500">Inventory</span> tab.
@@ -28,6 +31,19 @@
         <p class="m-0">
           5. Note that clicking the <span class="font-bold text-error">X</span> button beside an item on the list will
           remove it.
+        </p>
+      </Fieldset>
+
+      <Fieldset
+        legend="Header"
+        :toggleable="true"
+      >
+        <p class="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
         </p>
       </Fieldset>
     </div>
