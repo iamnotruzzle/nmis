ee3a68a3ee097e02eb6bdaf9c5aacf8aaa121ab2
iamnotruzzle <iamnotruzzle@gmail.com>
2025-01-28 11:31:13 +0800

    created ecg page with filter

--- app/Http/Controllers/Wards/Census/ECG/ECGController.php
+++ app/Http/Controllers/Wards/Census/ECG/ECGController.php
@@ -3,13 +3,53 @@
 namespace App\Http\Controllers\Wards\Census\ECG;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class ECGController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $from = Carbon::parse($request->from)->startOfDay();
+        $to = Carbon::parse($request->to)->endOfDay();
+
+        // get auth wardcode
+        $authWardcode = DB::select(
+            "SELECT TOP 1
+                l.wardcode
+            FROM
+                user_acc u
+            INNER JOIN
+                csrw_login_history l ON u.employeeid = l.employeeid
+            WHERE
+                l.employeeid = ?
+            ORDER BY
+                l.created_at DESC;
+            ",
+            [Auth::user()->employeeid]
+        );
+        $authCode = $authWardcode[0]->wardcode;
+
+        $census = DB::select(
+            "SELECT
+                htypser.tscode,
+                htypser.tsdesc,
+                SUM(logs.quantity) AS total_quantity,
+                SUM(logs.price_total) AS total_cost
+            FROM csrw_patient_charge_logs AS logs
+            JOIN htypser ON htypser.tscode = logs.tscode
+            WHERE logs.entry_at = '$authCode'
+            AND logs.itemcode = 'ECG'
+            AND CAST(logs.created_at AS DATE) BETWEEN '$from' AND '$to'
+            GROUP BY htypser.tscode, htypser.tsdesc;"
+        );
+
+        return Inertia::render('Wards/Census/ECG/Index', [
+            'census' => $census,
+        ]);
     }
 
     public function store(Request $request)
--- resources/js/Pages/Wards/Census/ECG/Index.vue
+++ resources/js/Pages/Wards/Census/ECG/Index.vue
@@ -1,147 +1,106 @@
 <template>
   <app-layout>
-    <Head title="NMIS - Patients" />
+    <Head title="NMIS - ECG report" />
 
     <div class="card">
-      <span class="text-xl text-900 font-bold text-primary">NO. OF PATIENTS: {{ patientsList.length }}</span>
-
+      <h2 class="text-center my-4">ECG Consumption Report</h2>
+      <div class="flex flex-row justify-content-center">
+        <div class="flex flex-column w-2">
+          <label
+            for="from"
+            class="mb-2"
+            >FROM</label
+          >
+          <Calendar
+            id="from"
+            v-model="from"
+            dateFormat="mm-dd-yy"
+            placeholder="mm-dd-yyyy"
+            showIcon
+            showButtonBar
+            :manualInput="true"
+            :hideOnDateTimeSelect="true"
+          />
+        </div>
+        <div class="mx-2"></div>
+        <div class="flex flex-column w-2">
+          <label
+            for="to"
+            class="mb-2"
+            >TO</label
+          >
+          <Calendar
+            id="to"
+            v-model="to"
+            dateFormat="mm-dd-yy"
+            placeholder="mm-dd-yyyy"
+            showIcon
+            showButtonBar
+            :manualInput="true"
+            :hideOnDateTimeSelect="true"
+          />
+        </div>
+      </div>
       <DataTable
         class="p-datatable-sm"
-        v-model:filters="filters"
-        :value="patientsList"
+        :value="censusList"
         paginator
         :rows="20"
         :rowsPerPageOptions="[20, 30, 40]"
-        dataKey="hpercode"
-        filterDisplay="row"
-        sortField="patient"
+        dataKey="tsdesc"
+        sortField="tsdesc"
         :sortOrder="1"
         removableSort
-        :globalFilterFields="['hpercode', 'department', 'patient', 'status', 'physician']"
         showGridlines
       >
         <template #header>
-          <div class="flex flex-wrap align-items-center justify-content-end">
-            <!-- <div>
-              <v-icon
-                name="ri-user-shared-fill"
-                class="pi pi-send text-green-500 text-xl mr-2"
-              ></v-icon>
-              <span>:&nbsp;&nbsp;FOR DISCHARGE</span>
-            </div> -->
-
-            <div>
-              <div class="p-inputgroup">
-                <span class="p-inputgroup-addon">
-                  <i class="pi pi-search"></i>
-                </span>
-                <InputText
-                  id="searchInput"
-                  v-model="filters['global'].value"
-                  placeholder="Search"
-                />
-              </div>
-            </div>
-          </div>
+          <div class="flex flex-wrap align-items-center justify-content-end"></div>
         </template>
-        <template #empty> No patients found. </template>
-        <template #loading> Loading patients data. Please wait. </template>
+        <template #empty> No data found. </template>
+        <template #loading> Loading data. Please wait. </template>
         <Column
-          field="department"
-          header="DEPARTMENT"
+          field="tscode"
+          header="TSCODE"
           style="width: 5%"
           sortable
         >
           <template #body="{ data }">
             <div class="flex flex-row align-items-center">
-              {{ data.department }}
+              {{ data.tscode }}
             </div>
           </template>
         </Column>
         <Column
-          field="hpercode"
-          header="HOSP. #"
+          field="tsdesc"
+          header="Type of service"
           style="width: 5%"
         >
           <template #body="{ data }">
             <div class="flex flex-row align-items-center">
               <div class="">
-                <span class="text-green-500">{{ data.hpercode }}</span>
+                <span class="text-green-500">{{ data.tscode }}</span>
               </div>
             </div>
           </template>
         </Column>
         <Column
-          field="patient"
-          header="PATIENT"
-          sortable
-          style="width: 15%"
-        >
-          <template #body="{ data }">
-            <div class="flex flex-row align-items-center">
-              <div class="">
-                <span>{{ data.patient }}</span>
-              </div>
-            </div>
-          </template>
-        </Column>
-        <Column
-          header="ACTION"
-          style="width: 3%"
-        >
-          <template #body="{ data }">
-            <!-- {{ data }} -->
-            <div class="flex justify-content-center">
-              <Button
-                :class="data.logs_id == null ? 'pulse border-transparent' : 'bg-green-500 border-transparent'"
-                icon="pi pi-money-bill"
-                label="Bills"
-                @click="goToPatientCharge(data)"
-              />
-            </div>
-          </template>
-        </Column>
-        <Column
-          field="disposition"
-          header="DISPOSITION"
+          field="total_quantity"
+          header="Quantity"
           sortable
           style="width: 10%"
         >
           <template #body="{ data }">
-            <Tag
-              v-if="data.disposition == 'ADMIT'"
-              value="ADMITTED"
-              severity="info"
-            />
-            <Tag
-              v-else-if="data.disposition == 'TRASH'"
-              value="TREATED AND SENT HOME"
-              severity="success"
-            />
-            <Tag
-              v-else
-              value=""
-            ></Tag>
+            <span>{{ data.total_quantity }}</span>
           </template>
         </Column>
         <Column
-          field="physician"
-          header="PHYSICIAN"
+          field="total_cost"
+          header="Total cost"
           sortable
           style="width: 10%"
         >
           <template #body="{ data }">
-            <span>{{ data.physician }}</span>
-          </template>
-        </Column>
-        <Column
-          field="date"
-          header="DATE"
-          sortable
-          style="width: 10%"
-        >
-          <template #body="{ data }">
-            <span>{{ tzone(data.date) }}</span>
+            <span>{{ data.total_cost }}</span>
           </template>
         </Column>
       </DataTable>
@@ -189,7 +148,7 @@ export default {
     RadioButton,
   },
   props: {
-    patients: Object,
+    census: Object,
   },
   data() {
     return {
@@ -199,25 +158,20 @@ export default {
       rows: null,
       options: {},
       params: {},
-      patientsList: [],
-      filters: {
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        hpercode: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        department: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        patient: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        physician: { value: null, matchMode: FilterMatchMode.CONTAINS },
-      },
+      censusList: [],
+      from: null,
+      to: null,
     };
   },
   // created will be initialize before mounted
   created() {
-    this.totalRecords = this.patients.total;
-    this.params.page = this.patients.current_page;
-    this.rows = this.patients.per_page;
+    this.totalRecords = this.census.total;
+    this.params.page = this.census.current_page;
+    this.rows = this.census.per_page;
   },
   mounted() {
-    // console.log('mounted', this.patients);
-    this.storePatientsInContainer();
+    console.log('mounted', this.census);
+    this.storeCensusInController();
 
     this.loading = false;
   },
@@ -229,108 +183,70 @@ export default {
         return moment.tz(date, 'Asia/Manila').format('LLL');
       }
     },
-    setPatient(patient) {
-      const suffix = patient.patsuffix == null ? '' : patient.patsuffix;
-
-      return patient.patlast + ', ' + patient.patfirst + ' ' + patient.patmiddle + suffix;
+    getLocalDateString(utcStr) {
+      const date = new Date(utcStr);
+      return (
+        date.getFullYear() +
+        '-' +
+        String(date.getMonth() + 1).padStart(2, '0') +
+        '-' +
+        String(date.getDate()).padStart(2, '0') +
+        ' ' +
+        String(date.getHours()).padStart(2, '0') +
+        ':' +
+        String(date.getMinutes()).padStart(2, '0')
+      );
     },
-    // use storePatientsInContainer() function so that every time you make
-    // server request such as POST, the data in the table
-    // is updated
-    // storePatientsInContainer() {
-    //   this.patients.data.forEach((e) => {
-    //     this.patientsList.push({
-    //       enccode: e.enccode,
-    //       hpercode: e.hpercode,
-    //       patient: this.setPatient(e.patient),
-    //       bill_stat: e.patient.admission_date.patient_bill_stat.billstat,
-    //       //   los: ,
-    //       admission_date: e.patient.admission_date.admdate,
-    //       kg: this.checkIfBmiExistForWeight(e.patient.admission_date),
-    //       cm: this.checkIfBmiExistForHeight(e.patient.admission_date),
-    //       bmi: this.calculateBmi(e.patient.admission_date),
-    //       room_bed: e.room.rmname + ' - ' + e.bed.bdname,
-    //       physician: this.setPhysician(
-    //         e.patient.admission_date.physician,
-    //         e.patient.admission_date.physician2,
-    //         e.patient.admission_date.physician3,
-    //         e.patient.admission_date.physician4
-    //       ),
-    //     });
-    //   });
-    // },
-    storePatientsInContainer() {
-      console.log(this.patients);
-      this.patients.forEach((e) => {
-        this.patientsList.push({
-          logs_id: e.logs_id,
-          enccode: e.enccode,
-          hpercode: e.hpercode,
-          department: e.tsdesc,
-          patient:
-            e.patlast +
-            ',' +
-            ' ' +
-            e.patfirst +
-            ' ' +
-            (e.patmiddle == null ? '' : e.patmiddle) +
-            ' ' +
-            (e.patsuffix == null ? '' : e.patsuffix),
-          disposition: e.dispcode,
-          physician: e.lastname + ',' + ' ' + e.firstname + ' ' + (e.empsuffix == null ? '' : e.empsuffix),
-          date: e.erdtedis,
+    storeCensusInController() {
+      console.log(this.census);
+      this.census.forEach((e) => {
+        this.censusList.push({
+          tscode: e.tscode,
+          tsdesc: e.tsdesc,
+          total_quantity: e.total_quantity,
+          total_cost: e.total_cost,
         });
       });
     },
-    onPage(event) {
-      this.params.page = event.page + 1;
-      this.updateData();
-    },
     updateData() {
-      this.patientsList = [];
+      this.censusList = [];
       this.loading = true;
 
-      this.$inertia.get('wardspatients', this.params, {
+      this.$inertia.get('ecgreports', this.params, {
         preserveState: true,
         preserveScroll: true,
         onFinish: (visit) => {
-          this.totalRecords = this.patients.total;
-          this.patientsList = [];
-          this.storePatientsInContainer();
+          this.totalRecords = this.census.total;
+          this.censusList = [];
+          this.storeCensusInController();
           this.loading = false;
         },
       });
     },
-    goToPatientCharge(e) {
-      this.params.enccode = e.enccode;
-      this.params.patient = e.patient;
-      this.$inertia.get('patientcharge', this.params, {
-        preserveState: true,
-        preserveScroll: true,
-      });
+  },
+  watch: {
+    from: function (val) {
+      if (val != null) {
+        let from = this.getLocalDateString(val);
+        this.params.from = from;
+      } else {
+        this.params.from = null;
+        this.from = null;
+      }
+      this.updateData();
+    },
+    to: function (val) {
+      if (val != null) {
+        let to = this.getLocalDateString(val);
+        this.params.to = to;
+      } else {
+        this.params.to = null;
+        this.to = null;
+      }
+      this.updateData();
     },
   },
-  watch: {},
 };
 </script>
 
-<style scoped>
-@keyframes pulse {
-  0% {
-    transform: scale(1);
-  }
-
-  50% {
-    transform: scale(1.1);
-  }
-
-  100% {
-    transform: scale(1);
-  }
-}
-
-.pulse {
-  background-color: #d6ec13;
-  animation: pulse 1s infinite;
-}
-</style>
+<style scoped></style>
