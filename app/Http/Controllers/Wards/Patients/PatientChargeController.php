345f54d05842b4c1063fbeafb8c3cd64ad33a4b5
iamnotruzzle <iamnotruzzle@gmail.com>
2025-03-29 11:39:45 +0800

    added function to log void items in consumption tracker

--- app/Http/Controllers/Wards/Patients/PatientChargeController.php
+++ app/Http/Controllers/Wards/Patients/PatientChargeController.php
@@ -7,6 +7,7 @@ use App\Events\RequestStock;
 use App\Http\Controllers\Controller;
 use App\Jobs\ChargingWardConsumptionTrackerJobs;
 use App\Jobs\CreatePatientChargeLogsJobs;
+use App\Jobs\VoidingWardConsumptionTrackerJobs;
 use App\Models\AdmissionLog;
 use App\Models\CsrwCode;
 use App\Models\ERlog;
@@ -634,6 +635,13 @@ class PatientChargeController extends Controller
                             'quantity' => (int)$previousWardStocks->quantity + (int)$upd_QtyToReturn,
                         ]);
 
+                        $upd_QtyToReturn = (int)$upd_QtyToReturn;
+                        $upd_ward_stocks_id = $request->upd_ward_stocks_id;
+                        VoidingWardConsumptionTrackerJobs::dispatch(
+                            $upd_ward_stocks_id,
+                            $upd_QtyToReturn,
+                        );
+
                         PatientChargeReturnLogs::create([
                             'enccode' => $request->enccode,
                             'location' => $authWard,
@@ -715,88 +723,6 @@ class PatientChargeController extends Controller
                                 ]);
                         }
                     }
-                    // misc
-                    // else {
-                    //     $patientCharge = PatientCharge::where('enccode', $request->upd_enccode)
-                    //         ->where('itemcode', $request->upd_itemcode)
-                    //         ->where('pcchrgdte', $request->upd_pcchrgdte)
-                    //         ->first();
-                    //     $previousCharge = $patientCharge;
-
-                    //     $patientChargeLogs = PatientChargeLogs::where('id', $request->upd_id)->first();
-                    //     $previousPatientChargeLogs = $patientChargeLogs;
-
-                    //     // delete the patient charge log
-                    //     $patientChargeLogs->delete();
-
-                    //     if ((int)$previousPatientChargeLogs->quantity != (int)$upd_QtyToReturn) {
-                    //         PatientChargeLogs::create([
-                    //             'enccode' => $previousPatientChargeLogs->enccode,
-                    //             'acctno' => $previousPatientChargeLogs->acctno,
-                    //             'ward_stocks_id' => $previousPatientChargeLogs->ward_stocks_id,
-                    //             'itemcode' => $previousPatientChargeLogs->itemcode,
-                    //             'from' => $previousPatientChargeLogs->from,
-                    //             'manufactured_date' => Carbon::parse($previousPatientChargeLogs->manufactured_date)->format('Y-m-d H:i:s.v'),
-                    //             'delivery_date' => Carbon::parse($previousPatientChargeLogs->delivered_date)->format('Y-m-d H:i:s.v'),
-                    //             'expiration_date' => Carbon::parse($previousPatientChargeLogs->expiration_date)->format('Y-m-d H:i:s.v'),
-                    //             'quantity' => (int)$previousPatientChargeLogs->quantity - (int)$upd_QtyToReturn,
-                    //             'price_per_piece' => $previousPatientChargeLogs->price_per_piece,
-                    //             'price_total' => ((float)$previousPatientChargeLogs->quantity - (float)$upd_QtyToReturn) * (float)$previousPatientChargeLogs->price_per_piece,
-                    //             'pcchrgdte' => $previousPatientChargeLogs->pcchrgdte,
-                    //             'tscode' => $request->tscode,
-                    //             'entry_at' => $authWard,
-                    //             'entry_by' => $authID,
-                    //             'created_at' => $previousPatientChargeLogs->created_at,
-                    //             'updated_at' => $previousPatientChargeLogs->updated_at,
-                    //         ]);
-                    //     }
-
-                    //     if (((int)$previousCharge->pchrgqty - (int)$upd_QtyToReturn) == 0) {
-                    //         PatientCharge::where('enccode', $request->upd_enccode)
-                    //             ->where('itemcode', $request->upd_itemcode)
-                    //             ->where('pcchrgdte', $request->upd_pcchrgdte)
-                    //             ->delete();
-                    //     } else {
-                    //         PatientCharge::where('enccode', $request->upd_enccode)
-                    //             ->where('itemcode', $request->upd_itemcode)
-                    //             ->where('pcchrgdte', $request->upd_pcchrgdte)
-                    //             ->update([
-                    //                 'enccode' => $previousCharge->enccode,
-                    //                 'hpercode' => $previousCharge->hpercode,
-                    //                 'upicode' => null,
-                    //                 'pcchrgcod' => $previousCharge->pcchrgcod, // charge slip no.
-                    //                 'pcchrgdte' => $previousCharge->pcchrgdte,
-                    //                 'chargcode' => $previousCharge->chargcode, // type of charge (chrgcode from hcharge)
-                    //                 'uomcode' => $previousCharge->uomcode, // unit
-                    //                 'pchrgqty' =>  (int)$previousCharge->pchrgqty - (int)$upd_QtyToReturn,
-                    //                 'pchrgup' => $previousCharge->pchrgup,
-                    //                 'pcchrgamt' => ((float)$previousCharge->pchrgqty - (float)$upd_QtyToReturn) * (float)$previousCharge->pchrgup,
-                    //                 'pcstat' => 'A', // always A
-                    //                 'pclock' => 'N', // always N
-                    //                 'updsw' => 'N', // always N
-                    //                 'confdl' => 'N', // always N
-                    //                 'srcchrg' => $previousCharge->srcchrg,
-                    //                 'pcdisch' => 'Y',
-                    //                 'acctno' => $previousCharge->acctno, // SELECT * FROM hpatacct --pacctno
-                    //                 'itemcode' => $previousCharge->itemcode, // cl2comb or hmisc hmcode
-                    //                 'entryby' => $authID,
-                    //                 'orinclst' => null, // null
-                    //                 'compense' => null, // always null
-                    //                 'proccode' => null, // always null
-                    //                 'discount' => null, // always null
-                    //                 'disamt' => null, // always null
-                    //                 'discbal' => null, // always null
-                    //                 'phicamt' => null, // always null
-                    //                 'rvscode' => null, // always null
-                    //                 'licno' => null, // always null
-                    //                 'hpatkey' => null, // always null
-                    //                 'time_frequency' => null, // always null
-                    //                 'unit_frequency' => null, // always null
-                    //                 'qtyintake' => null, // always null
-                    //                 'uomintake' => null, // always null
-                    //             ]);
-                    //     }
-                    // }
                 } else {
                     // RETURN CONSUMABLE ITEMS
                     // dd($request);
---
+++ app/Jobs/VoidingWardConsumptionTrackerJobs.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\WardConsumptionTracker;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class VoidingWardConsumptionTrackerJobs implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    // jobs
+    public $tries = 5;
+
+    public $upd_ward_stocks_id;
+    public $upd_QtyToReturn;
+
+
+    public function __construct($upd_ward_stocks_id, $upd_QtyToReturn)
+    {
+        $this->upd_ward_stocks_id = $upd_ward_stocks_id;
+        $this->upd_QtyToReturn = $upd_QtyToReturn;
+    }
+
+    public function handle()
+    {
+        WardConsumptionTracker::where('wards_stocks_id', $this->upd_ward_stocks_id)
+            ->update([
+                'charged_qty' => DB::raw("charged_qty - {$this->upd_QtyToReturn}")
+            ]);
+    }
+
+    public function failed(\Throwable $e)
+    {
+        WardConsumptionTracker::where('wards_stocks_id', $this->upd_ward_stocks_id)
+            ->update([
+                'charged_qty' => DB::raw("charged_qty - {$this->upd_QtyToReturn}")
+            ]);
+    }
+}
