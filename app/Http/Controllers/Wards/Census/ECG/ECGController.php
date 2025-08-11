0b69360478c09583bba74dda7d68eae76d7c2f1e
iamnotruzzle <iamnotruzzle@gmail.com>
2025-08-11 10:55:41 +0800

    update

--- app/Http/Controllers/Wards/Census/ECG/ECGController.php
+++ app/Http/Controllers/Wards/Census/ECG/ECGController.php
@@ -55,6 +55,91 @@ class ECGController extends Controller
             ->groupBy('htypser.tscode', 'htypser.tsdesc')
             ->get();
 
+        // can also be Total no. Of ER patients
+        // $er_fee_monthly = DB::select(
+        //     "SELECT
+        //     htypser.tsdesc AS DEPARTMENT,
+        //     SUM(charge.pchrgqty) AS QUANTITY,
+        //     charge.pchrgup as 'UNIT COST',
+        //     SUM(charge.pcchrgamt) AS 'TOTAL COST'
+        // FROM hpatchrg AS charge
+        // JOIN herlog AS erlog ON erlog.enccode = charge.enccode
+        // JOIN htypser ON htypser.tscode = erlog.tscode
+        // WHERE charge.pcchrgcod LIKE 'ER%'
+        // AND charge.itemcode = 'EROPH' -- EROPH = Emergency room fee
+        // AND CAST(charge.pcchrgdte AS DATE) BETWEEN '2024-01-01' AND '2024-12-31'
+        // GROUP BY htypser.tsdesc, charge.pchrgup;"
+        // );
+
+        // can also be Total no of admitted at ER
+        // // change the toecode filter if needed
+        // $no_of_consultation_or_admission = DB::select(
+        // "SELECT
+        //     htypser.tsdesc AS DEPARTMENT,
+        //     COUNT(*) AS TOTAL_COUNT
+        // FROM henctr
+        // INNER JOIN herlog AS erlog ON erlog.enccode = henctr.enccode
+        // JOIN htypser ON htypser.tscode = erlog.tscode
+        // WHERE henctr.toecode = 'ER' -- ER CONSULTATION
+        // --WHERE henctr.toecode = 'ERADM' -- ER ADMISSION
+        // AND henctr.encdate BETWEEN '2023-01-01' AND '2023-12-31'
+        // GROUP BY htypser.tsdesc
+        // ORDER BY TOTAL_COUNT DESC;"
+        // );
+
+        // number of discharge at ER
+        // $no_of_discharge_at_er = DB::select(
+        //     "SELECT
+        //         htypser.tsdesc AS DEPARTMENT,
+        //         SUM(charge.pchrgqty) AS QUANTITY,
+        //         charge.pchrgup as 'UNIT COST',
+        //         SUM(charge.pcchrgamt) AS 'TOTAL COST'
+        //     FROM hpatchrg AS charge
+        //     JOIN herlog AS erlog ON erlog.enccode = charge.enccode
+        //     JOIN htypser ON htypser.tscode = erlog.tscode
+        //     WHERE charge.pcchrgcod LIKE 'ER%'
+        //     AND charge.itemcode = 'EROPH' -- EROPH = Emergency room fee
+        //     AND erlog.dispcode = 'TRASH'
+        //     AND CAST(charge.pcchrgdte AS DATE) BETWEEN '2025-03-01' AND '2025-03-31'
+        //     GROUP BY htypser.tsdesc, charge.pchrgup;"
+        // );
+
+        // query to get patient that overstayed in ER
+        // more than 4 hours meaning it overstays
+        // $overstayed_patients = DB::select(
+        //     "SELECT
+        //             htypser.tsdesc AS DEPARTMENT,
+        //             COUNT(*) AS TOTAL_OVERSTAYED,
+        //             CAST(AVG(DATEDIFF(SECOND, erlog.erdate, erlog.erdtedis)) / 3600.0 AS DECIMAL(10,2)) AS AVG_OVERSTAY_HOURS
+        //         FROM herlog AS erlog
+        //         JOIN htypser ON htypser.tscode = erlog.tscode
+        //         WHERE
+        //             erlog.erdtedis IS NOT NULL
+        //             AND erlog.erdate IS NOT NULL
+        //             AND erlog.erdate < erlog.erdtedis  -- Ensure valid stay period
+        //             AND DATEDIFF(MINUTE, erlog.erdate, erlog.erdtedis) > 240  -- More than 4 hours
+        //             AND erlog.erdate BETWEEN '2025-01-01' AND '2025-01-31'
+        //         GROUP BY htypser.tsdesc
+        //         ORDER BY htypser.tsdesc;"
+        // );
+
+        // get six hour range filter
+        // $six_hour_range_filter = DB::select(
+        //     "SELECT type.tsdesc AS Department,
+        //         RIGHT('0' + CAST(DATEPART(HOUR, erlog.erdate) / 6 * 6 AS VARCHAR(2)), 2) + ':00 - ' +
+        //         RIGHT('0' + CAST((DATEPART(HOUR, erlog.erdate) / 6 * 6 + 5) AS VARCHAR(2)), 2) + ':59' AS Six_Hour_Range,
+        //         COUNT(DISTINCT erlog.enccode) AS Total_No_Patients
+        //     FROM herlog AS erlog
+        //     JOIN htypser AS type ON type.tscode = erlog.tscode
+        //     WHERE erlog.erdate >= '2025-03-01' AND erlog.erdate < '2025-03-31'
+        //     GROUP BY
+        //         type.tsdesc,
+        //         DATEPART(HOUR, erlog.erdate) / 6
+        //     ORDER BY
+        //         Six_Hour_Range, Department;"
+        // );
+
+
         return Inertia::render('Wards/Census/ECG/Index', [
             'census' => $census,
         ]);
